5e5e6a14c2f368c5d170cc127fab241dcdb1368e
ADDRMeridan <maelmoulin@gmail.com>
2024-06-11 18:18:40 +0200

    fix: ajout conjured

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import java.util.Set;
 import java.util.function.Predicate;
 
 class GildedRose {
@@ -8,42 +9,50 @@ class GildedRose {
 
     public static final String BACKSTAGEPASS_NAME = "Backstage passes to a TAFKAL80ETC concert";
     public static final String BRIE_NAME = "Aged Brie";
+    public static final String CONJURED_NAME = "Conjured Mana Cake";
     public static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+
+    public static final Set<String> SPECIFIC_ITEMS = Set.of(BACKSTAGEPASS_NAME, BRIE_NAME, SULFURAS_NAME);
+    //Items that don't have a selling deadline and never see their quality change
+    public static final Set<String> ETERNAL_ITEMS = Set.of(SULFURAS_NAME);
+
     Item[] items;
+    private Predicate<Item> canBeRefined = item -> item.quality < QUALITY_UPPER_LIMIT;
+
+    private Predicate<Item> isEternalItem = item -> ETERNAL_ITEMS.contains(item.name);
+    private Predicate<Item> isSpecificItem = item -> SPECIFIC_ITEMS.contains(item.name);
+    private Predicate<Item> isConjuredItem = item -> CONJURED_NAME.equals(item.name);
 
     public GildedRose(Item[] items) {
+
         this.items = items;
+    }
+
+    private void updateNormalOrConjuredQuality(Item item) {
+
     }
 
     public void updateQuality() {
 
         for (Item item : items) {
-            //Normal & Sulfuras
-            if (!item.name.equals(BRIE_NAME)
-                    && !item.name.equals(BACKSTAGEPASS_NAME)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS_NAME)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+            if (isEternalItem.test(item)) {
+                continue; //We can skip eternal items as they don't change
+            }
+
+            if (!isSpecificItem.test(item)) {
+                updateQualityNormalOrConjuredItem(item);
             } else {
-                if (item.quality < QUALITY_UPPER_LIMIT) {
-                    //Brie + backstagepass refining
-                    item.quality = item.quality + 1;
+                //Brie + backstagepass refining
+                item.quality = item.quality + 1;
 
-                    if (item.name.equals(BACKSTAGEPASS_NAME)) {
-                        //Backstage only
-                        if (item.sellIn < 11) {
-                            if (item.quality < QUALITY_UPPER_LIMIT) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                if (item.name.equals(BACKSTAGEPASS_NAME)) {
+                    //Backstage only
+                    if (item.sellIn < 11) {
+                        item.quality = item.quality + 1;
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < QUALITY_UPPER_LIMIT) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                    if (item.sellIn < 6) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
@@ -56,24 +65,31 @@ class GildedRose {
             if (item.sellIn < 0) {
                 if (!item.name.equals(BRIE_NAME)) {
                     if (!item.name.equals(BACKSTAGEPASS_NAME)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS_NAME)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
+                        updateQualityNormalOrConjuredItem(item);
                     } else {
                         //Expired Backstagepass
                         item.quality = 0;
                     }
                 } else {
                     //Brie refining
-                    if (item.quality < QUALITY_UPPER_LIMIT) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = item.quality + 1;
                 }
             }
 
-            item.quality = Integer.max(0, item.quality);
+            //Manage Quality bounds
+            if (!SULFURAS_NAME.equals(item.name)) {
+                item.quality = Integer.min(QUALITY_UPPER_LIMIT, item.quality); //Max quality
+                item.quality = Integer.max(0, item.quality); //Min quality
+            }
+        }
+    }
+
+    private void updateQualityNormalOrConjuredItem(Item item) {
+
+        item.quality--;
+        if (isConjuredItem.test(item)) {
+            //Conjured Item degrades twice as fast
+            item.quality--;
         }
     }
 }
